cd819f07b5f04747d9e0b069c845ac7ec64095f3
charan sai <charansai3664@gmail.com>
2026-05-19 12:26:59 +0530

    Added dockerfile

---
+++ src/main/java/com/spring_crud_app/spring_crud/controller/CrudController.java
@@ -0,0 +1,15 @@
+package com.spring_crud_app.spring_crud.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+public class CrudController {
+    @GetMapping("")
+    public String getMethodName() {
+        return "hello from crud app";
+    }
+
+}
