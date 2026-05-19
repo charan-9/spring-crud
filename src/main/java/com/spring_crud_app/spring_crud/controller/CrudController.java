69148af595345aa5416449ae95120decacf061eb
charan sai <charansai3664@gmail.com>
2026-05-19 15:48:39 +0530

    Added Modifications

--- src/main/java/com/spring_crud_app/spring_crud/controller/CrudController.java
+++ src/main/java/com/spring_crud_app/spring_crud/controller/CrudController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CrudController {
     @GetMapping("")
     public String getMethodName() {
-        return "Coming from docker, built using jenkins";
+        return "Coming from docker, built using jenkins - freestyle";
     }
 
 }
