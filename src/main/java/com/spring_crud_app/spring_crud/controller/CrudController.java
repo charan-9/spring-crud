87b71fa4737a4bc9ea5ff79c24b7d11cc6a1660f
charan sai <charansai3664@gmail.com>
2026-05-19 15:46:16 +0530

    Modified Controller

--- src/main/java/com/spring_crud_app/spring_crud/controller/CrudController.java
+++ src/main/java/com/spring_crud_app/spring_crud/controller/CrudController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CrudController {
     @GetMapping("")
     public String getMethodName() {
-        return "hello from crud app after adding the webhook";
+        return "Coming from docker, built using jenkins";
     }
 
 }
